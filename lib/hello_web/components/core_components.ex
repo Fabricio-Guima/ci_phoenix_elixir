268292d71b5793f99591aaa296c9824e06196eb7
Fabricio_Guima <fsgkof@gmail.com>
2025-03-15 13:54:18 -0300

    remove alias

--- lib/hello_web/components/core_components.ex
+++ lib/hello_web/components/core_components.ex
@@ -17,7 +17,10 @@ defmodule HelloWeb.CoreComponents do
   use Phoenix.Component
   use Gettext, backend: HelloWeb.Gettext
 
+  alias Phoenix.HTML.Form
+
   alias Phoenix.LiveView.JS
+
 
   @doc """
   Renders a modal.
@@ -305,8 +308,9 @@ defmodule HelloWeb.CoreComponents do
   def input(%{type: "checkbox"} = assigns) do
     assigns =
       assign_new(assigns, :checked, fn ->
-        Phoenix.HTML.Form.normalize_value("checkbox", assigns[:value])
+        Form.normalize_value("checkbox", assigns[:value])
       end)
+
 
     ~H"""
     <div>
